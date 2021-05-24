4abccd74244464f34a0cd7d75213a54cb43defe0
Youness <std.youness@gmail.com>
2021-05-24 21:12:28 +0200

    Changed the req uri.

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ import 'index.css'
   
 console.log('Hello Youness')
 
-fetch('http://localhost:8000/youness')
+fetch('http://localhost:8000/')
 .then(response => response.json())
 .then(res => {
     document.querySelector('.title').innerHTML = "Python à renvoyé ce message: " + res.msg
