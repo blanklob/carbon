b3dbe565c31af0f801e8eb9026eb7e4430d8fde0
Youness <std.youness@gmail.com>
2021-06-08 14:30:06 +0200

    Created the cards v2.0

--- app/index.js
+++ app/index.js
@@ -1,13 +1,6 @@
 // SCSS imports
-import print from './classes/visualisation'
+import registerServiceWoker from './utils/sw'
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js').then(registration => {
-      console.log('SW registered: ', registration);
-    }).catch(registrationError => {
-      console.log('SW registration failed: ', registrationError);
-    });
-  });
-}
+
 console.log("Hello Developer.")
+// registerServiceWoker()
---
+++ app/utils/sw.js
@@ -0,0 +1,15 @@
+// Registring a service worker for PWA
+
+
+export default function registerServiceWoker() {
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/service-worker.js').then(registration => {
+        console.log('SW registered: ', registration);
+      }).catch(registrationError => {
+        console.log('SW registration failed: ', registrationError);
+      });
+    });
+  }
+}
+
