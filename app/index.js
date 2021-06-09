eb16f0ee81cb0bd018626a899dd2701229574618
Youness <std.youness@gmail.com>
2021-06-09 15:23:10 +0200

    Removed sw for dev.

--- app/index.js
+++ app/index.js
@@ -1,10 +1,8 @@
 import 'Styles/index.scss'
 import Preloader from './components/preloader'
-import registerServiceWoker from './utils/sw'
 
 class App {
   constructor() {
-    registerServiceWoker()
     this.preloader = new Preloader('.preloader')
   }
 }
