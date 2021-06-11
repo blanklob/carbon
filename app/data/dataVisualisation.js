6d36debaaa1557080e4d772c278fc036390f52f5
Mysterken <Mysterkenzo@gmail.com>
2021-06-11 16:26:34 +0200

    Merge branch 'dev' of https://github.com/younessidbakkasse/pfe-front into dev

--- app/data/dataVisualisation.js
+++ app/data/dataVisualisation.js
@@ -52,11 +52,56 @@ const text = svgByType.selectAll('text')
 
 
 // Graph II: By source
+const width = 450
+const height = 450
+const margin = 40
+
 const svgBySource = d3.select(".dashboard__bysource")
                   .append("svg")
-                  .attr("width", w)
-                  .attr("height", h)
+                  .attr("width", width)
+                  .attr("height", height)
+                  .append("g")
+                  .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
+var radius = Math.min(width, height) / 2 - margin
+
+const datasetBySource = [ 64, 9, 27 ]
+
+var color = d3.scaleOrdinal()
+    .domain(datasetByType)
+    .range(['#3EC865', '#D9EA75', '#EABB75']);
+
+var pie = d3.pie()
+  .value(function(d) {return d.value; })
+var data_ready = pie(d3.entries(datasetByType))
+
+var arcGenerator = d3.arc()
+  .innerRadius(0)
+  .outerRadius(radius)
+  .startAngle(function(d) { return d.startAngle + Math.PI; })
+  .endAngle(function(d) { return d.endAngle + Math.PI; });
+
+svgBySource
+  .selectAll('mySlices')
+  .data(data_ready)
+  .enter()
+  .append('path')
+    .attr('d', arcGenerator)
+    .attr('fill', function(d){ return(color(d.data.key)) })
+    .attr('class', 'chart-data')
+    .attr("stroke", "black")
+    .style("stroke-width", "2px")
+    .style("opacity", 0.7)
+
+svgBySource
+  .selectAll('mySlices')
+  .data(data_ready)
+  .enter()
+  .append('text')
+  .text(function(d){ return d.value + '%'})
+  .attr("transform", function(d) { return "translate(" + arcGenerator.centroid(d) + ")";  })
+  .style("text-anchor", "middle")
+  .style("font-size", 17)
 
 // Graph III: Dialy Twitter
 const svgDialyPollution = d3.select(".dashboard__dialypollution")
