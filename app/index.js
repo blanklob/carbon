bdf140f315db3dfb893c64ad7b11bb39bcde0bdd
Youness <std.youness@gmail.com>
2021-06-14 11:29:58 +0200

    Added sw and fixed some SEO.

--- app/index.js
+++ app/index.js
@@ -1,10 +1,13 @@
 import Preloader from './components/preloader'
+import registerServiceWoker from './utils/sw'
 
 class App {
   constructor() {
     this.preloader = new Preloader('.preloader')
   }
 }
+
+registerServiceWoker()
 
 // Routage basique
 document.querySelectorAll('.search__btn').forEach((item) => {
