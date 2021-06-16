0fc0311ad867dd89def722f5f1dfbe291a011448
Youness <std.youness@gmail.com>
2021-06-16 19:14:52 +0200

    Fixed btn issue.

--- app/data/getUsername.js
+++ app/data/getUsername.js
@@ -2,7 +2,7 @@ export default function () {
   let input = document.querySelector('.search__input')
   let btns = document.querySelectorAll('.search__btn')
 
-  btn.array.forEach(btn => {
+  btns.array.forEach(btn => {
     btn.onclick = () => {
       window.localStorage.setItem('username', input.value)
     }
