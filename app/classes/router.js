f83e6ea0e625f0ee95ab6f83bbc187d5fed20f66
Youness <std.youness@gmail.com>
2021-06-17 17:16:31 +0200

    Fixed http.

--- app/classes/router.js
+++ app/classes/router.js
@@ -4,7 +4,7 @@ export default class Router {
     this.btns.forEach((item) => {
       item.addEventListener('click', (e) => {
 
-        window.location.href = newRoutes
+        window.location.href = newRoute + '.html'
       })
     })
   }
